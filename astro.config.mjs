165215ca32f9453877adbd2e81f3e0fa50ba7942
ARISEN <rfalconeb@gmail.com>
2023-04-12 11:31:45 -0300

    deploy done

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ARISEN-DEV.github.io',
+  base: '/landing-base',
   integrations: [tailwind(), react()]
 });
